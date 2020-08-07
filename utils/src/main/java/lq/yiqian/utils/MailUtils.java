cf5e5a4c9fd09c72c5559fb38b9594420b5dc219
lq-0519 <lq-0519@931722548@qq.com>
2020-08-07 11:12:24 +0800

    -

--- utils/src/main/java/lq/yiqian/utils/MailUtils.java
+++ utils/src/main/java/lq/yiqian/utils/MailUtils.java
@@ -19,9 +19,6 @@ public final class MailUtils {
      */
     /* 发送验证信息的邮件 */
     public static boolean sendMail(String to, String text, String title) {
-        System.out.println("to = " + to);
-        System.out.println("text = " + text);
-        System.out.println("title = " + title);
         try {
             final Properties props = new Properties();
             props.put("mail.smtp.auth", "true");

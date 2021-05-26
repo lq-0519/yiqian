f607ea5d08db2e61791e892751a2b6b7a3d64f50
壹仟 <931722548@qq.com>
2021-05-26 10:56:23 +0800

    代码格式化

--- web/src/main/java/lq/yiqian/controller/SearchHistoryController.java
+++ web/src/main/java/lq/yiqian/controller/SearchHistoryController.java
@@ -4,26 +4,24 @@ import com.github.pagehelper.PageInfo;
 import lq.yiqian.domain.SearchHistory;
 import lq.yiqian.domain.TopSearch;
 import lq.yiqian.service.ISearchHistoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
+ * 查询记录
+ *
  * @author LQ
  * @create 2020-06-25 17:57
- */
-
-/**
- * 查询记录
  */
 @Controller
 @RequestMapping("/searchHistory")
 public class SearchHistoryController {
-    @Autowired
+    @Resource
     private ISearchHistoryService searchHistoryService;
 
     /**
@@ -34,10 +32,13 @@ public class SearchHistoryController {
     @RequestMapping("/showSearchHistory")
     public ModelAndView showSearchHistory(@RequestParam(name = "page", defaultValue = "1") Integer page) {
         ModelAndView modelAndView = new ModelAndView();
-        int todayTotal = searchHistoryService.findTodayTotal();// 查询今天的搜索记录数
-        int yesterdayTotal = searchHistoryService.findYesterdayTotal();// 查询昨天的搜索记录数
-        List<SearchHistory> searchHistories = searchHistoryService.findAllByPage(page, 40);// 查询出搜索记录
-        PageInfo pageInfo = new PageInfo(searchHistories);
+        // 查询今天的搜索记录数
+        int todayTotal = searchHistoryService.findTodayTotal();
+        // 查询昨天的搜索记录数
+        int yesterdayTotal = searchHistoryService.findYesterdayTotal();
+        // 查询出搜索记录
+        List<SearchHistory> searchHistories = searchHistoryService.findAllByPage(page, 40);
+        PageInfo<SearchHistory> pageInfo = new PageInfo<>(searchHistories);
         modelAndView.addObject("todayTotal", todayTotal);
         modelAndView.addObject("yesterdayTotal", yesterdayTotal);
         modelAndView.addObject("pageInfo", pageInfo);
@@ -53,8 +54,9 @@ public class SearchHistoryController {
     @RequestMapping("/showTopSearch")
     public ModelAndView showTopSearch(@RequestParam(name = "page", defaultValue = "1") Integer page) {
         ModelAndView modelAndView = new ModelAndView();
-        List<TopSearch> topSearches = searchHistoryService.findTopSearch(page, 20);// 查询出搜索记录
-        PageInfo pageInfo = new PageInfo(topSearches);
+        // 查询出搜索记录
+        List<TopSearch> topSearches = searchHistoryService.findTopSearch(page, 20);
+        PageInfo<TopSearch> pageInfo = new PageInfo<>(topSearches);
         modelAndView.addObject("pageInfo", pageInfo);
         modelAndView.setViewName("pages/topSearch");
         return modelAndView;
